86d6a493d8098ee60b8f0179bcd56a11cf54c398
mach5mach <mark.w.geise@gmail.com>
2017-02-19 19:09:44 -0500

    Game Preview started
    A game preview for a fake game1 has been started and showing on the
    start screen.

---
+++ js/game1.js
@@ -0,0 +1,28 @@
+function game1() {
+    var scene = new THREE.Scene();
+    var canvas = document.getElementById("game-preview");
+    var camera = new THREE.PerspectiveCamera( 75, 640/480, 0.1, 1000 );
+
+
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize( 640, 480 );
+    document.body.appendChild( renderer.domElement );
+
+    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    var cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+
+    camera.position.z = 5;
+
+    var render = function () {
+        requestAnimationFrame( render );
+
+        cube.rotation.x += 0.1;
+        cube.rotation.y += 0.1;
+
+        renderer.render(scene, camera);
+    };
+
+    render();
+}
